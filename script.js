040d30ee18fde7fbba0c6b1cb5028f7e9ca962be
Jose Murilo <murilojucas360@hotmail.com>
2023-01-03 20:05:50 -0300

    add auteration script

--- script.js
+++ script.js
@@ -58,16 +58,16 @@ buttonSubmit.addEventListener('click', (event) => {
             inputEmail.value = ''
             inputPassword.value = ''
         })
-    } else {
-        containerModal.style.display = 'flex'
-        modal.style.backgroundColor = '#333'
-        result.innerHTML = 'Verifique se seus dados estão corretos!'
-        result.style.fontSize = '2.4rem'
-        result.style.textAlign = 'center'
-        result.style.color = 'white'
+    // } else {
+    //     containerModal.style.display = 'flex'
+    //     modal.style.backgroundColor = '#333'
+    //     result.innerHTML = 'Verifique se seus dados estão corretos!'
+    //     result.style.fontSize = '2.4rem'
+    //     result.style.textAlign = 'center'
+    //     result.style.color = 'white'
         
-        close.addEventListener('click', () => {
-            containerModal.style.display = 'none'
-        })
-    }
+    //     close.addEventListener('click', () => {
+    //         containerModal.style.display = 'none'
+    //     })
+    // }
 })
